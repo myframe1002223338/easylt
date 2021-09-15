34bf3dda98bf1159bc19c40760737066145f99d2
李腾 <1002223338@qq.com>
2021-09-15 11:34:07 +0000

    删除文件 application/model/copy_test2.php

--- application/model/copy_test2.php
+++
@@ -1,9 +0,0 @@
-<?php
-$username = $request['username'];
-class Select_work{
-    public $work;
-    public function __construct(){
-        global $mysql_orm,$username;
-        $this->work = $mysql_orm->model('select')->from('work,copy')->where('username=&$'.$username.'&')->query();
-    }
-}
