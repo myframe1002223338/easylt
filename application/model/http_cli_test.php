39b6943d7bcde94e8e698c11bf2ac5e331779c32
李腾 <1002223338@qq.com>
2021-09-15 11:34:11 +0000

    删除文件 application/model/http_cli_test.php

--- application/model/http_cli_test.php
+++
@@ -1,5 +0,0 @@
-<?php
-$http_cli = new http_client;
-$data = $mysql_orm->model('select')->from('content,swoole')->where('username=&$swoole&')->query();
-$result =$http_cli->post($data);
-$response = ['code'=>200,'msg'=>'response success!','data'=>$result];
